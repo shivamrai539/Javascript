30cf824a5d243bc672673d40b7b81d740f3faeec
Shivam Rai <shivamrai70049@gmail.com>
2025-04-09 15:11:27 +0000

    variables updates

---
+++ basics/variables.js
@@ -0,0 +1,21 @@
+const accountId = 4565
+let accountName = 'John Doe'
+var accountPassword = "123456"
+accountCity = "Mathura"
+let accountstate
+
+accountname = 'shivam rai'
+accountPassword  = 'ankit293932'
+accountCity = 'Delhi'
+
+
+
+
+// accountId = 2455662 // Not allowed to change constant value. 
+
+console.log(accountId);
+console.log(accountName);
+
+
+console.table([accountId, accountName, accountPassword, accountCity, accountstate])
+
