5d9ad618e4f89051d6e48306dde0d616b505b82a
Shivam Rai <shivamrai70049@gmail.com>
2025-04-10 19:13:24 +0530

    more operations

--- basics/DataTypeConversion.js
+++ basics/DataTypeConversion.js
@@ -40,3 +40,43 @@ console.log(cvtobool); // return type is true but it is not a real boolean value
 // "" => false
 // "shivam" => true
 
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
