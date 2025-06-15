f43bb9c617f796c545f0dedb130d0ba1c57709c4
Shivam Rai <shivamrai70049@gmail.com>
2025-06-15 07:57:23 +0000

    datatype_comparison

---
+++ basics/datatype_comparisons.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);   // make sure that same datatype.
+// console.log("02" > 1); // true, string is converted to number
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); 
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);    // avoid these type of conversions.
+
+console.log("2" === 2); // false, strict equality checks type and value
+
+
